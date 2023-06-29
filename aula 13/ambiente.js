df04d091588233759581e3e8af66d4aa53ed2431
Marlon Cordeiro Costa <marlonccosta@gmail.com>
2023-06-29 16:34:03 -0300

    aula 13 exercício 1

---
+++ aula 13/ambiente.js
@@ -0,0 +1,23 @@
+var cont = 1
+do {
+    console.log (`Passo ${cont}`)
+    cont ++
+} while (cont <= 10)
+
+
+/*var cont = 1
+while(cont <= 50){
+    console.log(`Passo ${cont}`)
+    cont ++
+}*/
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
+
