93486b62290cf5eca9b20cd4970207d8c6a80751
Marlon Cordeiro Costa <marlonccosta@gmail.com>
2023-07-10 17:55:36 -0300

    update função finalizar

--- aula16ex/ex18/script.js
+++ aula16ex/ex18/script.js
@@ -38,9 +38,19 @@ function finalizar(){
         window.alert("adicione valores antes de finaliza!!")
     }else {
         let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        for (let pos in valores){
+            if (valores[pos] > maior)
+            maior = valores[pos]
+            if (valores[pos] < menor)
+            menor = valores[pos]
+        }
 
         res.innerHTML = ' '
         res.innerHTML += `<p> Ao todo, temos ${tot} números castrasdos </p>`
+        res.innerHTML += `<p> O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p> O menor valor informado foi ${menor}.</p>`
     }
 }
     
